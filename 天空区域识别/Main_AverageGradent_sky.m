2487b72567283bc1db967115419a00746b9930fc
aomanhao <1014325364@qq.com>
2020-03-06 23:18:03 +0800

    update sky

--- 天空区域识别/Main_AverageGradent_sky.m
+++ 天空区域识别/Main_AverageGradent_sky.m
@@ -8,12 +8,10 @@ clear
 close all
 clc
 %% %%%%%%%%%%%%%%%图像%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% I=imread('Haze.jpg');
-% I=imread('tiananmen1.png');
-I=imread('foggy_bench.jpg');
+img=imread('foggy_bench.jpg');
 
-if size(I,3) == 3
-   I=rgb2gray(I);
+if size(img,3) == 3
+   I=rgb2gray(img);
 else
 end
 I=im2double(I);
@@ -27,7 +25,6 @@ M(M>0.6)=1;
 figure;imshow(M);title('(阈值');
 se=3; % the parameter of structuing element used for morphological reconstruction
 data_M=w_recons_CO(M,strel('square',se));
-
 figure;imshow((data_M));title('yuzhi');
 
 %%  梯度法
@@ -50,16 +47,12 @@ figure;imshow((I_gradient));title('梯度法');
 figure;imshow((data));title('梯度法');
 abs_DATA = abs(data-1);figure;imshow((abs_DATA));title('梯度');
 
-
+%% 融合
 K=imadd(data_M,abs_DATA,'uint16');        %使用imadd函数进行图像融合
 figure,imshow(K,[])         %显示融合后的图
 
 
-%% 融合
-
-% X1=double(X1)/255;  %这里转化成double类型，否则使用小波变换输出的会有大量大于1的存在，会导致图像显示有问题
-% X1=rgb2gray(X1); ?%原本以为小波变换只能使用一维的，看来可以使用3维
-
+%% 小波融合
 [c1,s1]=wavedec2(data_M,2,'sym4'); %将x1进行2维，使用‘sym4’进行变换
 
 sizec1=size(c1);
@@ -75,28 +68,4 @@ xx=waverec2(c,s,'sym4');  %进行重构
 figure;
 imshow(xx),title('融合后的');
 
-
-%% 平均梯度
-I=double(I);
-% [M,N,K]=size(I);
-sum=0;
-for i=1:m-1
-    for j=1:n-1
-        diffX(i,j)=I(i,j)-I(i+1,j);
-        diffY(i,j)=I(i,j)-I(i,j+1);
-        w(i,j)=sqrt(((diffX(i,j))^2+(diffY(i,j))^2)/2);
-        sum=sum+w(i,j);
-    end
-end
-
-
-
-diffX_a = mapminmax(diffX);
-figure;imshow(diffX_a);
-diffY_a = mapminmax(diffY);
-figure;imshow(diffY_a);
-W_a = mapminmax(w);
-figure;imshow(W_a);
-
-AVEGRAD=sum/((m-1)*(n-1));
 
