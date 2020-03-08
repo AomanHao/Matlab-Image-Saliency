937317b77661c6d63064ec333d0c4af7550f577f
aomanhao <1014325364@qq.com>
2020-03-08 21:01:48 +0800

    update sky

--- 天空区域识别/Main_AverageGradent_sky.m
+++ 天空区域识别/Main_AverageGradent_sky.m
@@ -8,6 +8,8 @@ clear
 close all
 clc
 %% %%%%%%%%%%%%%%%图像%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% I=imread('Haze.jpg');
+% I=imread('tiananmen1.png');
 img=imread('foggy_bench.jpg');
 
 if size(img,3) == 3
@@ -18,6 +20,9 @@ I=im2double(I);
 figure;imshow(I);title('(a)原始图像')
 [m,n]=size(I);
 
+% imhist(I);
+% tabulate(I(:))%分布概率
+
 %% 阈值
 M=I;
 M(M<=0.6)=0;
@@ -25,34 +30,44 @@ M(M>0.6)=1;
 figure;imshow(M);title('(阈值');
 se=3; % the parameter of structuing element used for morphological reconstruction
 data_M=w_recons_CO(M,strel('square',se));
-figure;imshow((data_M));title('yuzhi');
+
+figure;imshow((data_M));title('去噪阈值');
 
 %%  梯度法
 T=0.02;%阈值
-I_gradient=zeros(m,n);
+I_gradient=zeros(m,n);I_grad=zeros(m,n);
 for i=2:m-1
     for j=2:n-1
         I_gradient(i,j)=abs(I(i+1,j)-I(i,j))+abs(I(i,j+1)-I(i,j));
         if I_gradient(i,j)<T
-            I_gradient(i,j)=0;
+            I_grad(i,j)=0;
         else
-            I_gradient(i,j)=1;
+            I_grad(i,j)=1;
         end
     end
 end
 
 se=3; % the parameter of structuing element used for morphological reconstruction
-data=w_recons_CO(I_gradient,strel('square',se));
-figure;imshow((I_gradient));title('梯度法');
-figure;imshow((data));title('梯度法');
-abs_DATA = abs(data-1);figure;imshow((abs_DATA));title('梯度');
+data=w_recons_CO(I_grad,strel('square',se));
+figure;imshow((I_gradient));title('梯度信息');
+% imhist(I_gradient);
+% tabulate(I_gradient(:))%分布概率
+figure;imshow((I_grad));title('梯度图');
+figure;imshow((data));title('去噪梯度图');
+abs_DATA = abs(data-1);figure;imshow((abs_DATA));title('去噪梯度');
 
-%% 融合
+% data1=w_recons_CO(I_gradient,strel('square',se));
+% tabulate(data1(:))
+%% 加权融合
 K=imadd(data_M,abs_DATA,'uint16');        %使用imadd函数进行图像融合
 figure,imshow(K,[])         %显示融合后的图
 
 
-%% 小波融合
+%% 融合
+
+% X1=double(X1)/255;  %这里转化成double类型，否则使用小波变换输出的会有大量大于1的存在，会导致图像显示有问题
+% X1=rgb2gray(X1); ?%原本以为小波变换只能使用一维的，看来可以使用3维
+
 [c1,s1]=wavedec2(data_M,2,'sym4'); %将x1进行2维，使用‘sym4’进行变换
 
 sizec1=size(c1);
@@ -68,4 +83,4 @@ xx=waverec2(c,s,'sym4');  %进行重构
 figure;
 imshow(xx),title('融合后的');
 
-
+out = darkDefog(img);
